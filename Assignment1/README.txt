d8e4b6a6cda476cb0e56239d2932cd76b3ab90db
Mert SIDE <mertside@gmail.com>
2021-09-09 18:22:08 -0500

    optimized, cleaned, documented

--- Assignment1/README.txt
+++ Assignment1/README.txt
@@ -1,3 +1,16 @@
-nBlockingCommV5.c is the version that was deemed correct by Dr. Zhuang as of SEP 7th, 2021.
+CS5379: Parallel Processing - Assignment 1
 
--Mert
+by Tyler JOHNSON and Mert SIDE
+on 20210907 
+
+Result:
+Half of the calculation on pid == 1 overlaps with half of the communication.
+
+Procedure:
+pid == 1 sends the row sums between 0 and 25 as soon as they are calculated.
+pid == 0 receives these row sums between 0 and 25 right after its data 
+generation is completed. pid == 0 then continues in the meantime.
+pid == 1 sends the row sums between 25 and 50 after they are calculated. 
+pid == 0 receives the sums and prints everything.
+
+NOTE: implementation was deemed correct by Dr. Zhuang as of SEP 7th, 2021.
---
+++ Assignment1/old/nBlockingCommV6.c
@@ -0,0 +1,117 @@
+// CS5379: Parallel Processing - Assignment 1
+// 
+// by Tyler JOHNSON and Mert SIDE
+// on 20210907
+// 
+// Description: Move generation of i=0->50 to PID 1 in order to allow parallel
+// generation of the data. This also removed the sending and recieved for the
+// first 50 rows of the data. Before the recieving was blocking on PID 1.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
+
+#define generate_data(i,j)  (i)+(j)*(j)
+
+int main(int argc, char **argv)
+{
+  int i, j, pid, np, mtag, count;
+  double t0, t1;
+  int data[100][100], row_sum[100];
+  MPI_Status status; /* struct MPI_Status: MPI_SOURCE, MPI_TAG, MPI_ERROR */
+  MPI_Request req_s, req_r, req_a;
+
+  MPI_Init(&argc, &argv);              /* Set up */
+  MPI_Comm_rank(MPI_COMM_WORLD, &pid); /* Local process index */
+  MPI_Comm_size(MPI_COMM_WORLD, &np);  /* Totak processes */
+  
+  char name[80];
+  int length;
+  MPI_Get_processor_name(name, &length);
+
+  printf("\nHello, MPI! Rank: %d \nTotal: %d \nMachine: %s\n", pid, np, name);
+
+  if(pid == 0) {			/*** pid == 0 ***/
+    // generate data[] from 0 to 49
+    for(i = 0; i < 50; i++)
+      for(j = 0; j < 100; j++)
+        data[i][j] = i + j * j;
+    
+    // send data[] from 0 to 49
+    mtag = 1;
+    MPI_Isend(data, 2500, MPI_INT, 1, mtag, MPI_COMM_WORLD, &req_s);
+    
+    // generate data[] from 50 to 99
+    for(i = 50; i < 100; i++)
+      for(j = 0; j < 100; j++)
+        data[i][j] = generate_data(i,j);
+
+    mtag = 4;
+    MPI_Isend(&data[2500], 2500, MPI_INT, 1, mtag, MPI_COMM_WORLD, &req_a);
+    
+    /*** receive computed row_sums from  pid 1 ***/
+    mtag = 3 ;
+    MPI_Recv(row_sum, 25, MPI_INT, 1, mtag, MPI_COMM_WORLD, &status);
+
+    // sum data[] from 50 to 99
+    for(i = 50; i < 100; i++) {
+      row_sum[i] = 0;
+      for(j=0; j<100; j++)
+         row_sum[i] += data[i][j];
+    }
+
+    MPI_Wait(&req_s, &status);
+    MPI_Wait(&req_a, &status);
+
+    /*** receive computed row_sums from  pid 1 ***/
+    mtag = 2 ;
+    MPI_Recv(row_sum, 50, MPI_INT, 1, mtag, MPI_COMM_WORLD, &status);
+    
+    printf("\n");
+    // now we have all the sum from 0 to 99
+    for(i = 0; i < 100; i++) { 
+      printf(" %d  ", row_sum[i]);
+      if(i % 5 == 4) printf("\n");
+    }
+    printf("\n");
+
+  } else {				/*** pid == 1 ***/
+    // receive data[] from 0 to 49
+    mtag = 1 ;
+    // MPI_Recv(data, 5000, MPI_INT, 0, mtag, MPI_COMM_WORLD, &status);
+    MPI_Recv(data, 2500, MPI_INT, 0, mtag, MPI_COMM_WORLD, &status);
+
+    // sum data[] from 0 to 24
+    for(i = 0; i < 25; i++) {
+      row_sum[i] = 0;
+      for(j = 0; j < 100; j++)
+         row_sum[i] += data[i][j];
+    }
+    
+    /*** Send first 25 computed row_sums to pid 0 ***/
+    mtag = 3;
+    MPI_Isend(row_sum, 25, MPI_INT, 0, mtag, MPI_COMM_WORLD, &req_r);
+    
+    mtag = 4;
+    MPI_Recv(&data[2500], 2500, MPI_INT, 0, mtag, MPI_COMM_WORLD, &status);
+
+    // sum data[] from 25 to 49
+    for(i = 25; i < 50; i++) {
+      row_sum[i] = 0;
+      for(j = 0; j < 100; j++)
+         row_sum[i] += data[i][j];
+    }
+    
+    MPI_Wait(&req_r, &status);
+
+    /*** Send remaining computed row_sums to pid 0 ***/
+    mtag = 2;
+    MPI_Send(row_sum, 50, MPI_INT, 0, mtag, MPI_COMM_WORLD);
+         
+  }
+
+  MPI_Finalize();			/* Tear down */
+
+  return 0;
+} /****************** End of function main() ********************/
