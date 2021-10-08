a0399dc38c485a65c9da296bcdb7f08c9894a5a7
Tyler Johnson <tylermackj@gmail.com>
2021-10-08 18:26:15 -0500

    Getting correct result but there is an infinite loop

--- Assignment3/dijkstraRev1.c
+++ Assignment3/dijkstraRev1.c
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
   MPI_Init( &argc, &argv );
 
   int source = 3;
-  int n = 6;
+  int n = 20;
   int **edge = (int**)malloc(sizeof(int*) * n);
   int *distance = (int*)malloc(sizeof(int) * n);
   
@@ -38,12 +38,33 @@ int main(int argc, char** argv)
     }
   }
 
-  printEdge(edge, n);
-  printDistance(distance, n);
+  //printEdge(edge, n);
 
+  // Get non-parallel distances
+  printDistance(distance, n);
   dijkstra(source, n, edge, distance);
-
   printDistance(distance, n);
+
+  // Reset distances
+  for (int i =0; i < n; i++)
+  {
+    distance[i] = 987654321;
+  }
+
+  // Get parallel distances
+  printDistance(distance, n);
+  dijkstraParallel(source, n, edge, distance);
+  printDistance(distance, n);
+
+  // Free memory
+  free(distance);
+  /*
+  for (int i = 0; i < n; i++)
+  {
+    free(edge[i]);
+  }
+  free(edge);
+  */
 
   MPI_Finalize();
   return 0;
@@ -78,8 +99,6 @@ void dijkstra(int SOURCE, int n, int **edge, int *distance)
       }
     }
 
-    // collect all leasts to find global least
-
     found[leastPos] = 1;
     count++;
     // update the distances for all  nodes
@@ -97,13 +116,21 @@ void dijkstra(int SOURCE, int n, int **edge, int *distance)
 
 void dijkstraParallel(int SOURCE, int n, int **edge, int *distance)
 {
-  int i, j, count, tmp, least, leastPos, *found, pid, np;
+  int i, j, count, tmp, least, leastPos, *found, pid, np, *allLeasts, *allLeastPos;
+  MPI_Status status;
 
   // get MPI information
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-    // initialization...
+  // find process chunk
+  int start = (pid * n) / np;
+  int end = (((pid + 1) * n) / np) - 1;
+
+  // initialization...
+  allLeasts = (int*)malloc(sizeof(int) * np);
+  allLeastPos = (int*)malloc(sizeof(int) * np);
+
   found = (int *) calloc(n, sizeof(int));
   for(i = 0; i < n; i++) 
   {
@@ -118,7 +145,7 @@ void dijkstraParallel(int SOURCE, int n, int **edge, int *distance)
     least = 987654321; // infinitly large distance
 
     // find the minimum distance vertex
-    for(i = 0; i < n; i++) 
+    for(i = start; i < end; i++) 
     {
       tmp = distance[i];
       if( (!found[i]) && (tmp < least) ) 
@@ -128,12 +155,51 @@ void dijkstraParallel(int SOURCE, int n, int **edge, int *distance)
       }
     }
 
-    // collect all leasts to find global least
+    // collect all leasts and leastPos to find global least and leastPos
+
+    // send own least and leastPos to all other processes
+
+    for (int i = 0; i < np; i++)
+    {
+      if (i != pid)
+      {
+        MPI_Send(&least, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // Send least to process i
+        MPI_Send(&leastPos, 1, MPI_INT, i, 1, MPI_COMM_WORLD); // Send leastPos to process i
+      }
+    }
+
+    // collect leasts and leastPos from all other processes
+    for (int i = 0; i < np; i++)
+    {
+      if (i != pid)
+      {
+        MPI_Recv(&(allLeasts[i]), 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status); // Recv least from process i and put in ith position in allLeasts
+        MPI_Recv(&(allLeastPos[i]), 1, MPI_INT, i, i, MPI_COMM_WORLD, &status); // Recv leastPos from process i and put in ith position in allLeastPos
+      }
+    }
+
+    // append own least and leastPos to the list
+    allLeasts[pid] = least;
+    allLeastPos[pid] = leastPos;
+
+    // get least of allLeasts
+    int leastAllLeastPos = 0;
+    for (int i = 1; i < np; i++) 
+    {
+      if(allLeasts[leastAllLeastPos] > allLeasts[i])
+      {
+        leastAllLeastPos = i;
+      }
+    }
+
+    // save global least and least pos
+    least = allLeasts[leastAllLeastPos];
+    leastPos = allLeastPos[leastAllLeastPos];
 
     found[leastPos] = 1;
     count++;
     // update the distances for all  nodes
-    for(i = 0; i < n; i++) 
+    for(i = start; i < end; i++) 
     {
       if( (!found[i]) )
       {
@@ -141,7 +207,7 @@ void dijkstraParallel(int SOURCE, int n, int **edge, int *distance)
       }
     }
   } /*** End of while ***/
-
+  free(allLeasts);
   free(found); 
 }
 
